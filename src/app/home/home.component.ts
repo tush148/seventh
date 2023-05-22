8fb8a165ccf67e59adb3216a04d7c4e1d2b2896d
tush148 <129268239+tush148@users.noreply.github.com>
2023-05-22 18:50:06 +0530

    ms

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+// import { TypewriterState } from 'typewriter-effect';
+// import { Typewriter } from "node_modules/t-writer.js/dist/t-writer.js";
+// import { TypewriterClass } from 'typewriter-effect';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  }
